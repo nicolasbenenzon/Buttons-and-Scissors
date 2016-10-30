a26ceb98e716a6090b26c109f132d652ac6ee596
AgustinIzaguirre <Agustinizag1@gmail.com>
2016-10-30 18:04:13 -0300

    Update menu.c
    Agregue el llamado y cambie el return value de menu porque tenia retornar la opcion.
     la funcion no la agregue por que esta en el otro archivo despues cuando unimos todo la copiamos de ahi

--- menu.c
+++ menu.c
@@ -1,13 +1,13 @@
-void Menu();
+int Menu();
 void ImprimirMenu();
 
-void Menu(){
+int Menu(){
     ImprimirMenu();
-    // Acá agus agregá tu función LeerOpcion
+    int opcion=LeerOpcion();
 }
 
 void ImprimirMenu(){
-    printf("MENÚ DEL JUEGO:\n");
+    printf("MENÃš DEL JUEGO:\n");
     printf("1. Juego de dos jugadores\n");
     printf("2. Juego contra computadora\n");
     printf("3. Recuperar un juego grabado\n");
