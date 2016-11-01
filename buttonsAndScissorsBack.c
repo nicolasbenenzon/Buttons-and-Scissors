8f824b14117d58d000da67b2f200317564348bea
AgustinIzaguirre <Agustinizag1@gmail.com>
2016-11-01 09:33:59 -0300

    Update buttonsAndScissorsBack.c

--- buttonsAndScissorsBack.c
+++ buttonsAndScissorsBack.c
@@ -17,23 +17,26 @@ typedef struct
 	char tab[5][5];
 	int dim;
 }tablero;
-/*Busca botones del mismo tipo al inicial en la direccion de los incrementos y devuelve la cantidad*/ 
-int buscdir(tablero t,int modo,int fila,int columna,int incfil,int inccol)
+/*Busca botones del mismo tipo al inicial en la direccion de los incrementos y devuelve en su nombre la cantidad de blancos*/ 
+int buscdir(tablero t,int modo,int fila,int columna,int incfil,int inccol,int *botones)
 {
-	int seguir=1,botones=1;
+	*botones=1;
+	int seguir=1,blancos=0;
 	char boton=(t.tab)[fila][columna];
 	while(seguir)
 	{
 		fila+=incfil;
 		columna+=inccol;
-		if(fila>=t.dim||fila<0||columna>=t.dim||columna<0||(t.tab)[fila][columna]!=boton)
+		if(fila>=t.dim||fila<0||columna>=t.dim||columna<0||(((t.tab)[fila][columna]!=boton)&&((t.tab)[fila][columna]!=' ')))
 			seguir=0;
-		else if(modo==1&&botones==2)
+		else if(modo==1&&(*botones)==2)
 			seguir=0;
+		else if((t.tab)[fila][columna]==boton)
+			(*botones)++;
 		else
-			botones++;
+			blancos++;
 	}
-	return botones;
+	return blancos;
 }
 /*Elije al azar una de las dos estrategias(maxima cantidad de botones y minima cantidad de botes) y elige el movimiento aleatoriamente de todos los posibles*/
 void JugarAi(tablero* t,int *puntos)
@@ -45,49 +48,52 @@ void JugarAi(tablero* t,int *puntos)
 	{
 		for(j=0;j<t->dim;j++)
 		{
-			for(k=0;k<4;k++)
-			{
-				if(modo==0)
+			if((t->tab)[i][j]!=' ')
+			{for(k=0;k<4;k++)
 				{
-					if((botones=buscdir(*t,modo,i,j,direcciones[k][0],direcciones[k][1]))>=maximo)
+					if(modo==0)
 					{
-						if(botones>maximo)
+						blancos=buscdir(*t,modo,i,j,direcciones[k][0],direcciones[k][1],&botones);
+						if(botones>=maximo)
 						{
-							maximo=botones;
-							dimension=0;
-						}
+							if(botones>maximo)
+							{
+								maximo=botones;
+								dimension=0;
+							}
 
+						}
+						else
+							botones=0;
 					}
 					else
-						botones=0;
-				}
-				else
-					{
-						if(buscdir(*t,modo,i,j,direcciones[k][0],direcciones[k][1])==2)
-							botones=2;
-						else
-						 botones=0;
-					}
-				if(botones)
-				{
-					if(cant%BLOQUE==0)
-					{
-						aux=realloc(cortes,(cant+BLOQUE)*sizeof(*cortes));
-						if(aux!=NULL)
 						{
-							cortes=aux;
+							blancos=buscdir(*t,modo,i,j,direcciones[k][0],direcciones[k][1],&botones);
+							if(botones!=2)
+								botones=0;
 						}
-						else exit(0);
+					if(botones)
+					{
+						if(cant%BLOQUE==0)
+						{
+							aux=realloc(cortes,(cant+BLOQUE)*sizeof(*cortes));
+							if(aux!=NULL)
+							{
+								cortes=aux;
+							}
+							else exit(0);
+						}
+						cortes[dimension].inicio.fila=i;
+						cortes[dimension].inicio.columna=j;
+						cortes[dimension].final.fila=i+direcciones[k][0]*(botones-1+blancos);
+						cortes[dimension].final.columna=j+direcciones[k][1]*(botones-1+blancos);
+						dimension++;
+						if(dimension>cant)
+							cant=dimension;
 					}
-					cortes[dimension].inicio.fila=i;
-					cortes[dimension].inicio.columna=j;
-					cortes[dimension].final.fila=i+direcciones[k][0]*(botones-1);
-					cortes[dimension].final.columna=j+direcciones[k][1]*(botones-1);
-					dimension++;
-					if(dimension>cant)
-						cant=dimension;
-				}
-			}	
+
+				}	
+			}
 		}
 	}
 	cortes=realloc(cortes,sizeof(*cortes)*dimension);
